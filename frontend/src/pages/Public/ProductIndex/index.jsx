69576cfcf29c30616e1155ad564ac994155c28cd
Pietro Lanza <pietrolp333@gmail.com>
2023-08-31 12:59:52 -0300

    integração com a api de produtos feita

--- frontend/src/pages/Public/ProductIndex/index.jsx
+++ frontend/src/pages/Public/ProductIndex/index.jsx
@@ -2,9 +2,142 @@ import Cards from '../../../components/Product/Cards/Cards'
 import Navbar from '../../../components/Product/Navbar/Navbar'
 import NavItems from '../../../components/Product/Navbar/NavItems'
 import style from './style.module.css'
+
+import React from 'react'
 import { useRef } from 'react'
+import { useEffect, useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
+
+import BaseApi from '../../../services/Api'
+import { useStateContext } from '../../../context/ContextProvider'
+
+import { toast } from 'react-toastify'
+import Pagination from '../../../components/Layout/Pagination'
+
+const INITIAL_DATA = {
+    total: 0,
+    current_page: 1,
+    last_page: 1,
+    first_page_url: "",
+    last_page_url: "",
+    next_page_url: "",
+    prev_page_url: null,
+    path: "",
+    from: 1,
+    to: 1,
+    data: []
+};
 
 export default function ProductIndex() {
+     let [searchParams, setSearchParams] = useSearchParams();
+
+    let controller = new AbortController();
+
+    const INITIAL_FILTERS = {
+        search: searchParams.get("search") || "",
+    };
+
+    const INITIAL_QUERY = {
+        sort: "id",
+        order: "desc",
+        per_page: 10,
+        page: searchParams.get("page") || 1,
+    };
+
+    const [isLoading, setLoading] = React.useState(true);
+    const [isFiltering, setFiltering] = React.useState(true);
+    const [isPaginating, setPaginating] = React.useState(true);
+
+    const [query, setQuery] = React.useState({
+        ...INITIAL_QUERY,
+        ...INITIAL_FILTERS,
+    });
+    const [filters, setFilters] = React.useState({ ...INITIAL_FILTERS });
+    const [tableData, setTableData] = React.useState({ ...INITIAL_DATA });
+    const [natData, setNatData] = React.useState({ ...INITIAL_DATA });
+
+    const { setNotification } = useStateContext();
+
+    const requestData = (args = {}) => {
+        setLoading(true);
+        let q = { ...query, ...args };
+        BaseApi.get("/produto", {
+        signal: controller.signal,
+        params: {
+            ...q,
+            search: q.search !== "" ? q.search : undefined,
+        },
+        })
+        .then((response) => {
+            setTableData(response.data);
+            setLoading(false);
+            setPaginating(false);
+            setFiltering(false);
+        })
+        .catch((err) => {
+            if (err) {
+            console.log(err);
+            toast.error("Erro ao carregar dados da tabela");
+            setTableData({ ...INITIAL_DATA });
+            setLoading(false);
+            setPaginating(false);
+            setFiltering(false);
+            }
+        });
+    };
+
+    const requestNatData = () => {
+        setLoading(true);
+        BaseApi.get("/categoria")
+        .then((response) => {
+            setNatData(response.data);
+            setLoading(false);
+            setPaginating(false);
+            setFiltering(false);
+        })
+        .catch((err) => {
+            if (err) {
+            console.log(err);
+            toast.error("Erro ao carregar dados da tabela");
+            setNatData({ ...INITIAL_DATA });
+            setLoading(false);
+            setPaginating(false);
+            setFiltering(false);
+            }
+        });
+    };
+
+    const setSearch = (args = {}) => {
+        let params = { ...args };
+        if (filters.search && filters.search !== "") params.q = filters.search;
+        setSearchParams(params);
+      };
+    
+      const handlePagination = (page) => {
+        setSearch({ p: page });
+        setPaginating(true);
+        setQuery({ ...query, page });
+      };
+    
+      const handleFilters = (e) => {
+        e.preventDefault();
+        setSearch();
+        setFiltering(true);
+        setQuery({ ...query, ...filters, page: 1 });
+
+      };
+
+    useEffect(() => {
+        controller = new AbortController();
+        requestData();
+        requestNatData()
+       
+        return () => {
+        controller.abort();
+        setLoading(true);
+        };
+    }, [query]);
+
     let current_item = 0;
     const items = [
         "https://c4.wallpaperflare.com/wallpaper/999/730/463/yellow-fiction-cap-pikachu-detective-hd-wallpaper-preview.jpg",
@@ -53,6 +186,7 @@ export default function ProductIndex() {
 
             <div className={style.shoppingPage}>
                 <div className={style.slider}>
+                    <h1>Conheça algumas de nossas marcas parceiras:</h1>
                     <div className={style.gallery_wrapper}>
                         <div className={style.gallery}>
                             <button className={style.arrow_left} onClick={handleChangeImage} ref={leftArrow}>◄</button>
@@ -67,55 +201,60 @@ export default function ProductIndex() {
                         <h1>Categorias</h1>
                         
                         <div className={style.input_category}>
-                        <input type='checkbox' id='1'/>
-                        <label htmlFor='1'>Pelúcias</label>  
+                            <input type='checkbox' id='1'/>
+                            <label htmlFor='1'>Pelúcias</label> 
+
+                            <input type='checkbox' id='2'/>
+                            <label htmlFor='2'>Chaveiro</label> 
                         </div> 
                     </div>
 
-                    <div className={style.wrapper}>
-                        <Cards 
-                            img="https://m.media-amazon.com/images/I/415QqZN5DBL._AC_.jpg"
-                            nome="Pelúcia Pikachu"
-                            descricao="Uma pelúcia do Pikachu usando fantasia do Charizard"
-                            quantidade="15"
-                            categoria="Pelúcia"
-                        />
-                        <Cards 
-                            img="https://m.media-amazon.com/images/I/415QqZN5DBL._AC_.jpg"
-                            nome="Pelúcia Pikachu"
-                            descricao="Uma pelúcia do Pikachu usando fantasia do Charizard"
-                            quantidade="15"
-                            categoria="Pelúcia"
-                        />
-                        <Cards 
-                            img="https://m.media-amazon.com/images/I/415QqZN5DBL._AC_.jpg"
-                            nome="Pelúcia Pikachu"
-                            descricao="Uma pelúcia do Pikachu usando fantasia do Charizard"
-                            quantidade="15"
-                            categoria="Pelúcia"
-                        />
-                        <Cards 
-                            img="https://m.media-amazon.com/images/I/415QqZN5DBL._AC_.jpg"
-                            nome="Pelúcia Pikachu"
-                            descricao="Uma pelúcia do Pikachu usando fantasia do Charizard"
-                            quantidade="15"
-                            categoria="Pelúcia"
-                        />
-                        <Cards 
-                            img="https://m.media-amazon.com/images/I/415QqZN5DBL._AC_.jpg"
-                            nome="Pelúcia Pikachu"
-                            descricao="Uma pelúcia do Pikachu usando fantasia do Charizard"
-                            quantidade="15"
-                            categoria="Pelúcia"
-                        />
-                        <Cards 
-                            img="https://m.media-amazon.com/images/I/415QqZN5DBL._AC_.jpg"
-                            nome="Pelúcia Pikachu"
-                            descricao="Uma pelúcia do Pikachu usando fantasia do Charizard"
-                            quantidade="15"
-                            categoria="Pelúcia"
-                        />
-                    </div>
+                    {!isLoading && (
+                        <>
+                            {isFiltering && (
+                                <div className="spinner-border" role="status">
+                                <span className="visually-hidden">Loading...</span>
+                                </div>
+                            )}
+                            {!isFiltering && (
+                                <>
+                                    <Pagination
+                                        onPaginate={handlePagination}
+                                        showOnBottom={!isPaginating && tableData.data.length > 0}
+                                        showOnTop={tableData.data.length > 0}
+                                        paginateData={tableData}
+                                    >
+                                        
+                                        {!isPaginating && (
+                                            <div className={style.wrapper}>
+                                                {tableData.data.map ((product) => (
+                                                    <Cards
+                                                        img={product.imagem}
+                                                        nome={product.nome}
+                                                        descricao={product.descricao}
+                                                        quantidade={product.quantidade}
+                                                        categoria={product.categoria_id}
+                                                    />
+                                                ))}
+                                            </div>
+                                        )}
+
+                                        {isPaginating && (
+                                        <div className="spinner-border" role="status">
+                                            <span className="visually-hidden">Loading...</span>
+                                        </div>
+                                        )}
+                                        
+                                        {tableData.data.length === 0 && (
+                                        <h5 className="text-purple-3 text-center">
+                                            Não foram encontrados registros com estes filtros.
+                                        </h5>
+                                        )}
+                                    </Pagination>
+                                </>
+                            )}
+                        </>
+                    )}
                 </div>
             </div>
         </div>
